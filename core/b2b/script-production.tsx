79d65dfc33e90022a0d03cd38f01bdc83ef3f7b8
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-07-17 17:07:46 -0500

    Custom production Buyer Portal configuration

--- core/b2b/script-production.tsx
+++ core/b2b/script-production.tsx
@@ -11,33 +11,47 @@ interface Props {
   token?: string;
   environment: 'staging' | 'production';
   cartId?: string | null;
+  assetsVersion: string;
 }
 
-export function ScriptProduction({ cartId, storeHash, channelId, token, environment }: Props) {
+export function ScriptProduction({ cartId, storeHash, channelId, token, environment, assetsVersion }: Props) {
   useB2BAuth(token);
   useB2BCart(cartId);
 
+  const B2B_LOADER_URL = `https://store-${storeHash}.mybigcommerce.com/content/b2b-${assetsVersion}`;
+
   return (
     <>
-      <Script id="b2b-config">
+      <Script>
         {`
-            window.B3 = {
-              setting: {
-                store_hash: '${storeHash}',
-                channel_id: ${channelId},
-                platform: 'catalyst',
-                cart_url: '/cart',
-              }
-            }
+        window.b3CheckoutConfig = {
+          routes: {
+            dashboard: '/#/dashboard',
+          },
+        }
+        window.B3 = {
+          setting: {
+            store_hash: '${storeHash}',  
+            channel_id: ${channelId},
+          },
+        }
         `}
       </Script>
       <Script
-        data-channelid={channelId}
-        data-environment={environment}
-        data-storehash={storeHash}
-        src={`https://cdn.bundleb2b.net/b2b/${environment}/storefront/headless.js`}
         type="module"
-      />
+        crossOrigin=""
+        src={`${B2B_LOADER_URL}/index.js`}
+      ></Script>
+      <Script
+        noModule
+        crossOrigin=""
+        src={`${B2B_LOADER_URL}/polyfills-legacy.js`}
+      ></Script>
+      <Script
+        noModule
+        crossOrigin=""
+        src={`${B2B_LOADER_URL}/index-legacy.js`}
+      ></Script>
     </>
   );
 }
